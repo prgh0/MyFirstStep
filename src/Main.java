54afb94ff25cdcf8ead10a0ed01470669495d797
prgh0 <prgh00@mail.ru>
2026-07-26 01:16:24 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, я начинаю учить Java!");
+        System.out.println("ИГХТУ - это прошлое.");
+        int x = 5;
+        int y = 3;
+        System.out.println("Пять плюс три будет" + (x + y));
+    }
+}
